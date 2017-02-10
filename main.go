1634cda041ba168c135bf2b0fc2e1e963ef747d6
NexusLee <tonexus.lee@gmail.com>
2017-02-10 23:20:32 +0800

    去除代理

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 	//mux.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		req.Header.Add("Cookie", `TIEBA_USERTYPE=35f770fc37174a4733210804; TIEBAUID=4744b33b40f25e783e2053ed; bdshare_firstime=1461033889439; rpln_guide=1; BDUSS=J5eFpVTjc3cTJjWmwwcy1SWTkwY3k2T3VYZ0w4QUU1UFpIYnRZUnhsTjVqQ0JZQVFBQUFBJCQAAAAAAAAAAAEAAAA5DsMNwau7qLXE7r-w7QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHn~-Fd5~~hXZl; __cfduid=d49e1554629f98e985e49b4211d0e4ed01480938459; Hm_lvt_287705c8d9e2073d13275b18dbd746dc=1480150516,1481251799; BAIDUID=790368EB0B68D1F4C99B5CE8D201AA79:FG=1; PSTM=1483019965; BIDUPSID=F76E100A4FDCF8E145D344549AEDE87D; batch_delete_mode=true; STOKEN=7cdeb94b92fc441e4ebbeeaeeb4ce65c00d5ffbc16dd6b45c7ca9b3cb7cf7986; BDSFRCVID=JfKsJeCCxG36zJ6iqCwFOwAo_P_-JdjDIBG33J; H_BDCLCKID_SF=JJkO_D_atKvjDbTnMITHh-F-5fIX5-RLf2Qha-OF5lOTJh0RjR6kj4FLLpJlWbbCaaOTM56F5ncUOqojDTbke4tX-NFetjFJJf5; wise_device=0; BDRCVFR[feWj1Vr5u3D]=I67x6TjHwwYf0; PSINO=5; H_PS_PSSID=21871_1422_21086_21803`)
 		arr := make([]string, 1)
 
 		link := "http://tieba.baidu.com/f?kw=%D3%A2%D3%EF"
@@ -63,7 +64,7 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Println(topicNode)
+			//log.Println(topicNode)
 			arr = append(arr, topicNode)
 		})
 
@@ -113,14 +114,16 @@ func httpGet(url string) (content string, statusCode int) {
 }
 
 func ProxyFunc(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://tieba.baidu.com" + r.URL.String(), http.StatusFound)
+
 	// change the request host to match the target
-	r.Host = "tieba.baidu.com"
-	u, _ := url.Parse("http://tieba.baidu.com/")
-	proxy := httputil.NewSingleHostReverseProxy(u)
+	//r.Host = "tieba.baidu.com"
+	//u, _ := url.Parse("http://tieba.baidu.com/")
+	//proxy := httputil.NewSingleHostReverseProxy(u)
 	// You can optionally capture/wrap the transport if that's necessary (for
 	// instance, if the transport has been replaced by middleware). Example:
 	// proxy.Transport = &myTransport{proxy.Transport}
 	//proxy.Transport = &myTransport{}
 
-	proxy.ServeHTTP(w, r)
+	//proxy.ServeHTTP(w, r)
 }
